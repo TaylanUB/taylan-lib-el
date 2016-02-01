8c0642de7708eb6289d97a4560c3b696ec64aa9b
Taylan Ulrich Bayırlı/Kammer <taylanbayirli@gmail.com>
2016-02-01 10:47:27 +0100

    Add taylan-shell.el, sort requires in taylan-lib.el.

---
+++ taylan-shell.el
@@ -0,0 +1,35 @@
+;;; taylan-shell.el --- M-x shell addition.
+
+;; Copyright (C) 2014  Taylan Ulrich Bayirli/Kammer
+
+;; Author: Taylan Ulrich Bayirli/Kammer <taylanbayirli@gmail.com>
+;; Keywords: extensions, unix
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(require 'shell)
+
+(defun shell-new ()
+  "Creates a new shell buffer."
+  (interactive)
+  (shell (generate-new-buffer "*shell*")))
+
+(provide 'taylan-shell)
+;;; taylan-shell.el ends here
